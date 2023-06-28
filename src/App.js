1d7d03391295f0f8c9fcffe0623fb42764643f8a
Ish Kumar <Ish.Kumar@Geminisolutions.com>
2023-06-28 11:17:45 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Login from './Components/Login'
-import Logout from './Components/Logout'
-import Navbar from './Components/Navbar';
+import React from "react";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
+import Navbar from "./Components/Navbar";
 import "./App.css";
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
-const App=()=> {
-    const user=useSelector(selectUser);
-  return <div> 
-    <Navbar/>
-    {user ? <Logout/> : <Login/>}</div>;
-  
+const App = () => {
+  const user = useSelector(selectUser);
+  return (
+    <div>
+      <Navbar />
+      {user ? <Logout /> : <Login />}
+    </div>
+  );
 };
 
-export default App
+export default App;
